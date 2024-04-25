da28e21911c2ec4192bc99abddb001c85c825e29
A111221058 <A111221058@mail.shu.edu.tw>
2024-04-25 17:56:40 +0800

    0425

---
+++ ButtonHW_A111221058/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ButtonHW_A111221058"
+include(":app")
+ 
